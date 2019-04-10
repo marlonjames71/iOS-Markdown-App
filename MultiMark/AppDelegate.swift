d3fca0ae33ea4238ee11e657cce930d7002cc322
Michael Redig <mredig@gmail.com>
2019-04-10 17:02:57 -0500

    Finished tutorial i think

--- MultiMark/AppDelegate.swift
+++ MultiMark/AppDelegate.swift
@@ -9,13 +9,23 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
 
 	var window: UIWindow?
 
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
+		if let split = window?.rootViewController as? UISplitViewController {
+			split.delegate = self
+			split.preferredDisplayMode = .allVisible
+			split.maximumPrimaryColumnWidth = .greatestFiniteMagnitude
+			split.preferredPrimaryColumnWidthFraction = 0.5
+		}
+		return true
+	}
+	
+	func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController: UIViewController, onto primaryViewController: UIViewController) -> Bool {
 		return true
 	}
 
