107008142d1288292f271f0ae96e4fa9ffad9753
Michael Redig <mredig@gmail.com>
2019-04-10 15:48:20 -0500

    initial commit external screen works

--- MultiMark/ViewController.swift
+++ MultiMark/ViewController.swift
@@ -8,13 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate {
 
+	@IBOutlet var textView: UITextView!
+	var additionalWindows = [UIWindow]()
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+		
+		NotificationCenter.default.addObserver(forName: UIScreen.didConnectNotification, object: nil, queue: nil) { [weak self] (notification) in
+			guard let self = self else { return }
+			guard let newScreen = notification.object as? UIScreen else { return }
+			let screenDimensions = newScreen.bounds
+			
+			let newWindow = UIWindow(frame: screenDimensions)
+			newWindow.screen = newScreen
+			
+			guard let vc = self.storyboard?.instantiateViewController(withIdentifier: "PreviewViewController") as? PreviewViewController else {
+				fatalError("Unable to find PreviewViewController")
+			}
+			
+			newWindow.rootViewController = vc
+			newWindow.isHidden = false
+			self.additionalWindows.append(newWindow)
+		}
 	}
 
+	
+	func textViewDidChange(_ textView: UITextView) {
+		guard let preview = additionalWindows.first?.rootViewController as? PreviewViewController else { return }
+		
+		preview.text = textView.text
+	}
 
 }
 
