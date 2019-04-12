0221a899e921f41c7d47f6c45098409ac99a9e14
Michael Redig <mredig@gmail.com>
2019-04-12 16:56:45 -0500

    added css template changed to webview added html generator class

--- MultiMark/PreviewViewController.swift
+++ MultiMark/PreviewViewController.swift
@@ -8,25 +8,74 @@
 
 import UIKit
 import Down
+import WebKit
+import Html
 
-class PreviewViewController: UIViewController {
+class PreviewViewController: UIViewController, WKNavigationDelegate {
 
-	@IBOutlet var outputView: UITextView!
+	@IBOutlet var outputView: WKWebView!
+	
+	var fontSize = 100
+	
+	let css: String
 	
 	var text: String = "" {
 		didSet {
 			let down = Down(markdownString: text)
-			let style = "body { font: 200% sans-serif; }"
-			let attributedString = try? down.toAttributedString(stylesheet: style)
-			outputView.attributedText = attributedString
+//			let style = "body { font: 200% sans-serif; }"
+			let markdownHtml = (try? down.toHTML()) ?? ""
+			let doc: Node = .document(
+				.html(
+					.head(
+						.style(unsafe: css)
+					),
+					.body(
+						.raw(markdownHtml)
+					)
+				)
+			)
+			outputView.loadHTMLString(render(doc), baseURL: nil)
 		}
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		let cssUrl = Bundle.main.url(forResource: "modern", withExtension: "css")!
+		css = try! String(contentsOf: cssUrl, encoding: .utf8)
+		super.init(coder: aDecoder)
 	}
 	
 	override func viewDidLoad() {
         super.viewDidLoad()
-
+		configureSizeButtons()
+		outputView.navigationDelegate = self
         // Do any additional setup after loading the view.
     }
+	
+	func configureSizeButtons() {
+		navigationItem.rightBarButtonItems = [
+			UIBarButtonItem(title: "+", style: .plain, target: self, action: #selector(adjustSizeUp)),
+			UIBarButtonItem(title: "-", style: .plain, target: self, action: #selector(adjustSizeDown))
+		]
+	}
+	
+	@objc func adjustSizeUp() {
+		fontSize += 50
+		adjustSize()
+	}
+	
+	@objc func adjustSizeDown() {
+		fontSize -= 50
+		adjustSize()
+	}
+	
+	func adjustSize() {
+		let js = "document.getElementsByTagName('body')[0].style.webkitTextSizeAdjust='\(fontSize)%'"
+		outputView.evaluateJavaScript(js, completionHandler: nil)
+	}
+	
+	func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+		adjustSize()
+	}
     
 
     /*
